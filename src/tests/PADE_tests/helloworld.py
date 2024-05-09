2be210ef53ff32f5878b28315be0c5fcf79c841e
ekhurtado <e16hurtado@gmail.com>
2024-05-09 13:50:50 +0200

    Setup of prosody XMPP server to deploy in Kubernetes

---
+++ src/tests/PADE_tests/helloworld.py
@@ -0,0 +1,29 @@
+import time
+
+from pade.misc.utility import display_message, start_loop
+from pade.core.agent import Agent
+from pade.acl.aid import AID
+from sys import argv
+
+
+class AgenteHelloWorld(Agent):
+    def __init__(self, aid):
+        super(AgenteHelloWorld, self).__init__(aid=aid)
+        display_message(self.aid.localname, 'Hello World!')
+
+
+if __name__ == '__main__':
+
+    time.sleep(10) # wait to get logs in Kubernetes
+
+    agents_per_process = 3
+    c = 0
+    agents = list()
+    for i in range(agents_per_process):
+        port = int(argv[1]) + c
+        agent_name = 'agente_hello_{}@localhost:{}'.format(port, port)
+        agente_hello = AgenteHelloWorld(AID(name=agent_name))
+        agents.append(agente_hello)
+        c += 1000
+
+    start_loop(agents)
