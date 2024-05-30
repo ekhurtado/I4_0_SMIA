16b015ce6afcb389da314baa30633c582227b8ec
ekhurtado <e16hurtado@gmail.com>
2024-05-30 11:15:50 +0200

    test: Sphinx theme changed to Furo theme

--- src/AAS_Manager/docs/conf.py
+++ src/AAS_Manager/docs/conf.py
@@ -44,40 +44,42 @@ html_static_path = ['_static']
 # -----------------
 # READTHEDOCS THEME
 # ----------------
-html_theme = 'sphinx_rtd_theme'
-
-# Add the CSS file to use all width of the webpage (for sphinx_rtd_theme)
-html_css_files = [
-    'css/custom.css',
-]
-
-# Configuration of sphinx_rtd_theme HTML theme
-html_theme_options = {
-    "navigation_depth": 2,
-    "collapse_navigation": False,
-    "logo_only": True
-}
-
-html_logo = 'images/Component_I4_0_logo_positive.png'   # To add the logo
-html_show_sphinx = False    # To not show “Created using Sphinx” in the HTML footer
+# html_theme = 'sphinx_rtd_theme'
+#
+# # Add the CSS file to use all width of the webpage (for sphinx_rtd_theme)
+# html_css_files = [
+#     'css/custom.css',
+# ]
+#
+# # Configuration of sphinx_rtd_theme HTML theme
+# html_theme_options = {
+#     "navigation_depth": 2,
+#     "collapse_navigation": False,
+#     "logo_only": True
+# }
+#
+# html_logo = 'images/Component_I4_0_logo_positive.png'   # To add the logo
+# html_show_sphinx = False    # To not show “Created using Sphinx” in the HTML footer
 
 # ---------
 # FURO THEME
 # ----------
 # Documentation: https://pradyunsg.me/furo/customisation/fonts/
-# html_theme = 'furo'
-#
-# # Configuration of Furo HTML theme
-# html_logo = "images/Component_I4_0_logo_positive.png"
-# html_theme_options = {
-#     # "light_logo": "images/Component_I4_0_logo_positive.png",
-#     # "dark_logo": "images/Component_I4_0_logo_positive.png",
-#     "light_css_variables": {
-#         "color-brand-primary": "#34A1E8",
-#         "color-brand-content": "#2980B9",
-#         "color-problematic": "DodgerBlue",  # other CSS colors: https://www.w3schools.com/cssref/css_colors.php
-#         # "color-admonition-background": "#2980B9",
-#     },
-# }
-# pygments_dark_style = "monokai"
+html_theme = 'furo'
+
+# Configuration of Furo HTML theme
+html_logo = "images/Component_I4_0_logo_positive.png"
+html_theme_options = {
+    # "light_logo": "images/Component_I4_0_logo_positive.png",
+    # "dark_logo": "images/Component_I4_0_logo_positive.png",
+    "light_css_variables": {
+        "color-brand-primary": "#34A1E8",
+        "color-brand-content": "#2980B9",
+        "color-problematic": "DodgerBlue",  # other CSS colors: https://www.w3schools.com/cssref/css_colors.php
+        # "color-admonition-background": "#2980B9",
+    },
+}
+pygments_dark_style = "monokai"
+
+html_show_sphinx = False    # To not show “Created using Sphinx” in the HTML footer
 
