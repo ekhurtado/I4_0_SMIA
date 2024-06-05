4e09e910a9be94eff90715c58bae12436a2db457
ekhurtado <e16hurtado@gmail.com>
2024-06-05 16:02:31 +0200

    feat: AAS Core ROS working with tests using local JSON files

--- src/AAS_Cores/ROS_AAS_Core/src/utilities/Interactions_utils.py
+++ src/AAS_Cores/ROS_AAS_Core/src/utilities/Interactions_utils.py
@@ -19,9 +19,8 @@ def delete_svc_request(svc_request_object):
     print("Deleting the service request with ID " + str(svc_request_object['interactionID']) + " from AAS Archive.")
     svc_requests_file_path = AASarchiveInfo.MANAGER_INTERACTIONS_FOLDER_PATH + AASarchiveInfo.SVC_REQUEST_FILE_SUBPATH
     svc_requests_json = file_to_json(svc_requests_file_path)
-    # svc_requests_json['serviceRequests'].pop(svc_request_object)
-    for i in svc_requests_json['serviceRequests']:
-        if i['interactionID'] == svc_request_object['interactionID']:
-            svc_requests_json['interactionID'].pop(i)
+    for i in range(len(svc_requests_json['serviceRequests'])):
+        if svc_requests_json['serviceRequests'][i]["interactionID"] == svc_request_object['interactionID']:
+            svc_requests_json['serviceRequests'].pop(i)
             break
     update_json_file(svc_requests_file_path, svc_requests_json)
