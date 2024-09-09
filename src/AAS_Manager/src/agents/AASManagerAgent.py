e66cfe0e8d9da636fe8c585c1514401c43374b08
ekhurtado <e16hurtado@gmail.com>
2024-09-09 16:32:18 +0200

    test:AAS Manager attribute documentation in sphinx

--- src/AAS_Manager/src/agents/AASManagerAgent.py
+++ src/AAS_Manager/src/agents/AASManagerAgent.py
@@ -18,12 +18,11 @@ class AASManagerAgent(Agent):
     Agent will be the generic and from which all other types of AAS Managers will start.
     """
 
-    acl_svc_requests = {}   # Dictionary to save FIPA-ACL service requests
-    acl_svc_responses = {}   # Dictionary to save FIPA-ACL service responses
-
-    interaction_id_num = 0  # Identifier for Intra AAS interaction, created by the AAS Manager
-    interaction_requests = {}  # Dictionary to save Intra AAS interaction requests
-    interaction_responses = {}  # Dictionary to save Intra AAS interaction responses
+    acl_svc_requests = {}   #: Dictionary to save FIPA-ACL service requests
+    acl_svc_responses = {}   #: Dictionary to save FIPA-ACL service responses
+    interaction_id_num = 0  #: Identifier for Intra AAS interaction, created by the AAS Manager
+    interaction_requests = {}  #: Dictionary to save Intra AAS interaction requests
+    interaction_responses = {}  #: Dictionary to save Intra AAS interaction responses
 
     def __init__(self, jid: str, password: str, verify_security: bool = False):
         super().__init__(jid, password, verify_security)
