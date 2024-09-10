d888906af1a9d1887bd0f015d8d5961820959e34
ekhurtado <e16hurtado@gmail.com>
2024-09-10 18:13:28 +0200

    feat:added hover color to API documentation in sphinx

--- src/AAS_Manager/docs/conf.py
+++ src/AAS_Manager/docs/conf.py
@@ -85,12 +85,14 @@ html_theme_options = {
         "color-problematic": "DodgerBlue",  # other CSS colors: https://www.w3schools.com/cssref/css_colors.php
         # "color-admonition-background": "#2980B9",
         "color-api-background": "#E7F2FA",
+        "color-api-background-hover": "#a0a7ad",
     },
     "dark_css_variables": {  # colors for dark theme
         "color-brand-primary": "#34A1E8",
         "color-brand-content": "#2980B9",
         "color-problematic": "DodgerBlue",
         "color-api-background": "#383838",
+        "color-api-background-hover": "#848484",
     }
 }
 
