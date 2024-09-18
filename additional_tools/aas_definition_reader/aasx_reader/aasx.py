933387308cae6cf923a27110a802489e76df3006
ekhurtado <e16hurtado@gmail.com>
2024-09-18 18:26:56 +0200

    feat:progress with the code of the program to read AASX files and obtain the desired submodels and conceptdescriptions from the AAS definitions within them

--- additional_tools/aas_definition_reader/aasx_reader/aasx.py
+++ additional_tools/aas_definition_reader/aasx_reader/aasx.py
@@ -3,11 +3,16 @@ Functionality for reading and writing AASX files
 """
 import io
 import os
-from typing import IO, Union
+from typing import IO, Union, Any, Optional
 
 import pyecma376_2
 from basyx.aas.adapter.json import read_aas_json_file
 from basyx.aas.adapter.xml import read_aas_xml_file
+from lxml import etree
+
+# from xml_reader.aas_definition_reader import validate_xml_definition
+
+xsd_path = '../AAS_metamodel.xsd'
 
 
 class MyAASXReader:
@@ -32,6 +37,20 @@ class MyAASXReader:
         except Exception as e:
             raise ValueError("{} is not a valid ECMA376-2 (OPC) file: {}".format(file, e)) from e
 
+    def validate_xml_definition_file(self, xml_element):
+        xsd_aas_schema_file = etree.parse(xsd_path)
+        xsd_aas_schema = etree.XMLSchema(xsd_aas_schema_file)
+
+        if not xsd_aas_schema.validate(xml_element):
+            print("The XML definition is not valid. Reason:")
+            for error in xsd_aas_schema.error_log:
+                print("\t" + error.message)
+                print("\tLine:", error.line)
+                print("\tColumn:", error.column)
+                return False
+        else:
+            return True
+
     def _parse_aas_part(self, part_name: str, **kwargs) -> {}:
         """
         Helper function to parse the AAS objects from a single JSON or XML part of the AASX package.
@@ -54,5 +73,112 @@ class MyAASXReader:
                 return read_aas_json_file(io.TextIOWrapper(p, encoding='utf-8-sig'), **kwargs)
         else:
             print("Could not determine part format of AASX part {} (Content Type: {}, extension: {}"
-                         .format(part_name, content_type, extension))
+                  .format(part_name, content_type, extension))
             return {}  # TODO esto lo he cambiado
+
+    def _parse_xml_document(self, file: IO, failsafe: bool = True, **parser_kwargs: Any) -> Optional[etree.Element]:
+        """
+        Parse an XML document into an element tree
+
+        :param file: A filename or file-like object to read the XML-serialized data from
+        :param failsafe: If True, the file is parsed in a failsafe way: Instead of raising an Exception if the document
+                         is malformed, parsing is aborted, an error is logged and None is returned
+        :param parser_kwargs: Keyword arguments passed to the XMLParser constructor
+        :return: The root element of the element tree
+        """
+
+        parser = etree.XMLParser(remove_blank_text=True, remove_comments=True, **parser_kwargs)
+
+        try:
+            return etree.parse(file, parser).getroot()
+        except etree.XMLSyntaxError as e:
+            if failsafe:
+                print(e)
+                return None
+            raise e
+
+    def save_aas_xml_definition_objects(self, part_name: str, **kwargs) -> {}:
+        global_dict_object = []
+        content_type = self.reader.get_content_type(part_name)
+        extension = part_name.split("/")[-1].split(".")[-1]
+        if content_type.split(";")[0] in ("text/xml", "application/xml") or content_type == "" and extension == "xml":
+            print("Parsing AAS objects from XML stream in OPC part {} ...".format(part_name))
+            with self.reader.open_part(part_name) as definition_file:
+                root = self._parse_xml_document(definition_file, failsafe=True, **kwargs)
+                if root is None:
+                    return None
+
+                # if not self.validate_xml_definition_file(root):   # TODO de momento quitado ya que falla con alguno
+                #     print("The XML is not valid, it cannot be read.")
+
+                # Recogemos el namespace para que las busquedas no fallen
+                ns_aas = ''
+                if len(list(root.nsmap.keys())) != 0:
+                    ns_aas = '{' + list(root.nsmap.values())[0] + '}'
+                    # ns_aas = list(root.nsmap.keys())[0] + ':' # TODO opcion B, pero habria que añadir el nsmap en todos los "find" de elementos xml
+
+                print("Parsing AAS objects from XML definition...")
+                for object_list in root:
+                    if object_list.tag == '{https://admin-shell.io/aas/3/0}assetAdministrationShells':
+                        print("Getting all AAS objects")
+                        for aas_xml_object in object_list:
+                            aas_dict_object = {'xml_object': aas_xml_object,
+                                               'submodels': self.get_submodels_of_aas(ns_aas, aas_xml_object, root)
+                                               }
+                            global_dict_object.append(aas_dict_object)
+
+                return global_dict_object
+
+    def get_submodels_of_aas(self, ns_aas, aas_xml_object, root_object: etree.Element):
+        dict_submodels_object = []
+        submodels = aas_xml_object.find(ns_aas + "submodels")
+        if submodels is not None:
+            for submodel_ref in submodels:
+                # First, get the reference of the submodel
+                sm_ref_type = submodel_ref.find(ns_aas + "type")
+                if sm_ref_type == "ExternalReference":
+                    print("It cannot get the submodelo of an external reference")
+                    break
+                keys = submodel_ref.find(ns_aas + "keys")
+                for key in keys:
+                    key_value_text = self.get_xml_elem_text(key, ns_aas + "value")
+                    # Once the reference is obtained, it will search the submodel xml element in all AAS definition
+                    sm_xml_element = self.get_xml_element_by_id(root_object, ns_aas,
+                                                                'submodels', key_value_text)
+                    # The sm element is stored
+                    dict_submodels_object.append({'xml_object': sm_xml_element,
+                                                  'conceptDescriptions': self.get_conceptdescriptions_of_submodel(
+                                                      ns_aas, sm_xml_element, root_object)})
+        return dict_submodels_object
+
+    def get_conceptdescriptions_of_submodel(self, ns_aas, sm_xml_object, root_object: etree.Element):
+        dict_conceptdescriptions_object = []
+        conceptdescriptions_ref = sm_xml_object.iterfind(".//" + ns_aas + "semanticId")
+        for cd_ref in conceptdescriptions_ref:
+            keys = cd_ref.find(ns_aas + "keys")
+            for key in keys:
+                key_type_text = self.get_xml_elem_text(key, ns_aas + "type")
+                if key_type_text == 'ConceptDescription':
+                    key_value_text = self.get_xml_elem_text(key, ns_aas + "value")
+                    cd_xml_element = self.get_xml_element_by_id(root_object, ns_aas,
+                                                                'conceptDescriptions', key_value_text)
+                    if cd_xml_element is not None:
+                        dict_conceptdescriptions_object.append(cd_xml_element)
+        return dict_conceptdescriptions_object
+
+
+
+    def get_xml_element_by_id(self, root, ns_aas, elem_type, sm_reference_id):
+        submodels = root.find(ns_aas + elem_type)
+        if submodels is not None:
+            for submodel in submodels:
+                # The id of the submodel is obtained
+                sm_id_text = self.get_xml_elem_text(submodel, ns_aas + "id")
+                if sm_id_text == sm_reference_id:
+                    return submodel
+            return None
+        return None
+
+    def get_xml_elem_text(self, xml_elem, ns_aas):
+        found_element = xml_elem.find(ns_aas)
+        return found_element.text if found_element is not None else None
--- additional_tools/aas_definition_reader/aasx_reader/aasx_definition_reader.py
+++ additional_tools/aas_definition_reader/aasx_reader/aasx_definition_reader.py
@@ -66,7 +66,8 @@ def read_aasx_file(aasx_file_path):
         # TODO, con el aas_part ya tenemos el archivo de definicion del AAS, ahora hay que hacer un programa que lea
         #  los submodelos y deje elegir cual se quiere lograr. De momento esta la lectura de la definicion del AAS de
         #  Basyx
-        aas_part_object = aasx_reader._parse_aas_part(aas_part)
+        # aas_part_object = aasx_reader._parse_aas_part(aas_part)
+        aas_part_object = aasx_reader.save_aas_xml_definition_objects(aas_part)
         print(aas_part_object)
 
 
@@ -81,70 +82,6 @@ def get_aasx_reader(file):
         raise ValueError("{} is not a valid ECMA376-2 (OPC) file: {}".format(file, e)) from e
 
 
-def validate_xml_definition(aas_xml_definition_str):
-    # Para validarlo, utilizaremos el paquete lxml de Python
-    # Primero, leemos el schema oficial de AAS (meta-modelo)
-    xsd_aas_schema_file = etree.parse(xsd_path)
-    xsd_aas_schema = etree.XMLSchema(xsd_aas_schema_file)
-
-    aas_xml_bytes = aas_xml_definition_str.encode('utf-8')  # fromstring cannot handle strings with an encoding
-    # declaration when the string is already a Unicode string,
-    # it has to encode to bytes
-    aas_xml_definition = etree.fromstring(aas_xml_bytes)
-
-    # Validamos el XML contra el XSD oficial de AASs
-    is_valid = xsd_aas_schema.validate(aas_xml_definition)
-
-    if not is_valid:
-        print("The XML definition is not valid. Reason:")
-        for error in xsd_aas_schema.error_log:
-            print("\t" + error.message)
-            print("\tLine:", error.line)
-            print("\tColumn:", error.column)
-            exit()  # exit the program
-
-
-def read_xml_definition(aas_xml_definition_str):
-    # Primero, parseamos el string XML a objeto en Python
-    aas_xml_bytes = aas_xml_definition_str.encode('utf-8')  # fromstring cannot handle strings with an encoding
-    aas_xml_env_definition = etree.fromstring(aas_xml_bytes)
-
-    # Recogemos el namespace para que las busquedas no fallen
-    xml_ns = ''
-    if list(aas_xml_env_definition.nsmap.keys())[0] is not None:
-        xml_ns = list(aas_xml_env_definition.nsmap.keys())[0] + ':'
-
-    if aas_xml_env_definition is None:
-        print("XML not valid")
-        exit()
-    else:
-        # En este caso ya podemos empezar a leer el XML
-        print("XML definition is valid. Starting reading...")
-        # AAS related global information
-        aas_list_elem = aas_xml_env_definition.find(xml_ns + "assetAdministrationShells", aas_xml_env_definition.nsmap)
-        aas_elem = aas_list_elem.find(xml_ns + "assetAdministrationShell", aas_list_elem.nsmap)
-
-        asset_information_elem = aas_elem.find(xml_ns + "assetInformation", aas_elem.nsmap)
-        asset_information_obj = deserialization.deserialize_asset_information(asset_information_elem, xml_ns)
-
-        # Submodel related reference information
-        sm_reference_list_elem = aas_elem.find(xml_ns + "submodels", aas_elem.nsmap)
-        if sm_reference_list_elem is not None:
-            sm_reference_dict = deserialization.get_submodel_references(sm_reference_list_elem, xml_ns)
-            print("Submodels info")
-            print(sm_reference_dict)
-
-        # Submodel related information
-        sm_list_elem = aas_xml_env_definition.find(xml_ns + "submodels", aas_xml_env_definition.nsmap)
-        sm_obj_list = set()
-        for sm_elem in sm_list_elem:
-            sm_obj = deserialization.deserialize_submodel(sm_elem, xml_ns)
-            sm_obj_list.add(sm_obj)
-
-        aas_obj = deserialization.deserialize_aas(aas_elem, xml_ns, asset_information_obj, sm_obj_list)
-
-        print("CASCADE PRINTING:")
-        aas_obj.cascade_print()
 
 
 if __name__ == "__main__":
